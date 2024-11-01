694cd859c5e4fe69e3d9f2ea4d656ea5051c29b4
Anupamvats52 <vatsanupam321@gmail.com>
2024-11-01 20:51:24 +0530

    Add files via upload

---
+++ anagram.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class anagram {
+    public static void main(String[] args) {
+        // leetcode Qno.242
+        System.out.println(isanagram("earth","heart"));
+}
+
+public static boolean isanagram(String s,String t){
+     char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+}
+}
